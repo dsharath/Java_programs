35fa23b0a8698632fa5ea737471a54e5efc1fc7c
sharath chandra <dsharathc7@gmail.com>
2018-08-29 21:17:49 -0500

    doc: adding basic java programm

---
+++ src/main/java/com/sharath/java/demo/JavaproApplication.java
@@ -0,0 +1,12 @@
+package com.sharath.java.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaproApplication.class, args);
+	}
+}
