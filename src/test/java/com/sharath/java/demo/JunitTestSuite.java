a57444f3ce33441e299a63768ed0b37854124c48
sharath chandra <dsharathc7@gmail.com>
2018-09-04 12:22:19 -0500

    doc: Basic function using TestSuite

---
+++ src/test/java/com/sharath/java/demo/JunitTestSuite.java
@@ -0,0 +1,21 @@
+package com.sharath.java.demo;
+
+import junit.framework.TestResult;
+import junit.framework.TestSuite;
+
+public class JunitTestSuite {
+	
+	public static void main(String [] a) {
+		
+		TestSuite suite = new TestSuite(TestJunit1.class, TestJunit2.class, TestJunit3.class);
+		
+		TestResult result = new TestResult();
+		
+		suite.run(result);
+		
+		System.out.println("Number of test cases="+result.runCount());
+		
+		
+	}
+
+}
